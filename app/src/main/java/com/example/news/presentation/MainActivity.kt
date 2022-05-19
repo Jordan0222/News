9b9d48e81640e72cb29a880f68f2b8d6658b1098
asxz0 <asxz0121@gmail.com>
2022-05-19 16:57:18 +0800

    fix bottom navigation bar animated

--- app/src/main/java/com/example/news/presentation/MainActivity.kt
+++ app/src/main/java/com/example/news/presentation/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.FiberNew
 import androidx.compose.material.icons.filled.Language
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.compose.NavHost
-import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -34,82 +34,86 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NewsTheme {
-                val navController = rememberNavController()
-
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar(
-                            items = listOf(
-                                BottomNavItem(
-                                    name = "熱門新聞",
-                                    route = Screen.BreakingNewsScreen.route,
-                                    icon = Icons.Default.FiberNew
-                                ),
-                                BottomNavItem(
-                                    name = "焦點新聞",
-                                    route = Screen.CategoryNewsScreen.route,
-                                    icon = Icons.Default.Language
-                                ),
-                                /*BottomNavItem(
-                                    name = "珍藏",
-                                    route = Screen.SaveNewsScreen.route,
-                                    icon = Icons.Default.Bookmark
-                                ),*/
-                                BottomNavItem(
-                                    name = "搜尋",
-                                    route = Screen.SearchNewsScreen.route,
-                                    icon = Icons.Default.Search
-                                ),
-                            ),
-                            navController = navController,
-                            onItemClick = {
-                                navController.navigate(it.route)
-                            }
-                        )
-                    }
-                ) {
-                    Navigation(navController = navController)
-                }
+                BottomBarNavigation()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Navigation(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = Screen.BreakingNewsScreen.route
-    ) {
-        composable(
-            route = Screen.BreakingNewsScreen.route
-        ) {
-            BreakingNewsScreen(navController = navController)
-        }
+fun BottomBarNavigation() {
+    val bottomBarState = rememberSaveable { mutableStateOf(true) }
+    val navController = rememberNavController()
 
-        composable(
-            route = Screen.CategoryNewsScreen.route
-        ) {
-            CategoryNewsScreen(navController = navController)
-        }
-
-        composable(
-            route = Screen.SearchNewsScreen.route
-        ) {
-            SearchNewsScreen(navController = navController)
-        }
-
-        composable(
-            route = Screen.ArticleScreen.route + "?url={url}",
-            arguments = listOf(
-                navArgument("url") {
-                    type = NavType.StringType
-                    defaultValue = ""
-                    nullable = true
-                }
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                items = listOf(
+                    BottomNavItem(
+                        name = "熱門新聞",
+                        route = Screen.BreakingNewsScreen.route,
+                        icon = Icons.Default.FiberNew
+                    ),
+                    BottomNavItem(
+                        name = "焦點新聞",
+                        route = Screen.CategoryNewsScreen.route,
+                        icon = Icons.Default.Language
+                    ),
+                    /*BottomNavItem(
+                        name = "珍藏",
+                        route = Screen.SaveNewsScreen.route,
+                        icon = Icons.Default.Bookmark
+                    ),*/
+                    BottomNavItem(
+                        name = "搜尋",
+                        route = Screen.SearchNewsScreen.route,
+                        icon = Icons.Default.Search
+                    ),
+                ),
+                navController = navController,
+                bottomBarState = bottomBarState
             )
+        }
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = Screen.BreakingNewsScreen.route
         ) {
-            ArticleScreen()
+            composable(
+                route = Screen.BreakingNewsScreen.route
+            ) {
+                bottomBarState.value = true
+                BreakingNewsScreen(navController = navController)
+            }
+
+            composable(
+                route = Screen.CategoryNewsScreen.route
+            ) {
+                bottomBarState.value = true
+                CategoryNewsScreen(navController = navController)
+            }
+
+            composable(
+                route = Screen.SearchNewsScreen.route
+            ) {
+                bottomBarState.value = true
+                SearchNewsScreen(navController = navController)
+            }
+
+            composable(
+                route = Screen.ArticleScreen.route + "?url={url}",
+                arguments = listOf(
+                    navArgument("url") {
+                        type = NavType.StringType
+                        defaultValue = ""
+                        nullable = true
+                    }
+                )
+            ) {
+                bottomBarState.value = false
+                ArticleScreen()
+            }
         }
     }
 }
--- app/src/main/java/com/example/news/presentation/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/news/presentation/components/BottomNavigationBar.kt
@@ -1,8 +1,12 @@
 package com.example.news.presentation.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -10,6 +14,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 
 @ExperimentalMaterialApi
@@ -17,40 +22,55 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 fun BottomNavigationBar(
     items: List<BottomNavItem>,
     navController: NavController,
-    modifier: Modifier = Modifier,
-    onItemClick: (BottomNavItem) -> Unit
+    bottomBarState: MutableState<Boolean>,
+    modifier: Modifier = Modifier
 ) {
-    val backStackEntry = navController.currentBackStackEntryAsState()
-    BottomNavigation(
-        modifier = modifier,
-        backgroundColor = Color.White,
-        elevation = 5.dp
-    ) {
-        items.forEach { item ->
-            val selected = item.route == backStackEntry.value?.destination?.route
-            BottomNavigationItem(
-                selected = selected,
-                onClick = { onItemClick(item) },
-                selectedContentColor = Color(0xFF27AE60),
-                unselectedContentColor = Color.Gray,
-                icon = {
-                    Column(
-                        horizontalAlignment = CenterHorizontally
-                    ) {
-                        Icon(
-                            imageVector = item.icon,
-                            contentDescription = item.name
-                        )
-                        if (selected) {
-                            Text(
-                                text = item.name,
-                                textAlign = TextAlign.Center,
-                                fontSize = 12.sp
-                            )
+    AnimatedVisibility(
+        visible = bottomBarState.value,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it }),
+        content = {
+            val backStackEntry = navController.currentBackStackEntryAsState()
+            BottomNavigation(
+                modifier = modifier,
+                backgroundColor = Color.White,
+                elevation = 5.dp
+            ) {
+                items.forEach { item ->
+                    val selected = item.route == backStackEntry.value?.destination?.route
+                    BottomNavigationItem(
+                        selected = selected,
+                        onClick = {
+                              navController.navigate(item.route) {
+                                  popUpTo(navController.graph.findStartDestination().id) {
+                                      saveState = true
+                                  }
+                                  launchSingleTop = true
+                                  restoreState = true
+                              }
+                        },
+                        selectedContentColor = Color(0xFF27AE60),
+                        unselectedContentColor = Color.Gray,
+                        icon = {
+                            Column(
+                                horizontalAlignment = CenterHorizontally
+                            ) {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                                if (selected) {
+                                    Text(
+                                        text = item.name,
+                                        textAlign = TextAlign.Center,
+                                        fontSize = 12.sp
+                                    )
+                                }
+                            }
                         }
-                    }
+                    )
                 }
-            )
+            }
         }
-    }
+    )
 }
